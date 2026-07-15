52d6640cb3374ba4b6cc889d5d4dd2030cfd1c46
Kleyton Ferreira <kleyton.ferreira.kp@gmail.com>
2026-07-15 14:37:12 -0300

    modulo-01: ex07

---
+++ Modulo-01/ex07.py
@@ -0,0 +1,12 @@
+horas_trabalhas = float(input())
+valor_hora = float(input())
+
+excedido = float()
+
+if horas_trabalhas > 40:
+    excedido = horas_trabalhas - 40
+    horas_trabalhas = 40
+
+total = (horas_trabalhas * valor_hora) + (excedido * valor_hora) * 1.5
+
+print(total)
