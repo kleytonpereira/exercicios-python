baff2725c0de8f5139560153f954d54499e573a3
Kleyton Ferreira <kleyton.ferreira.kp@gmail.com>
2026-07-17 14:41:23 -0300

    Modulo-03: fix ex43

--- Modulo-03/ex43.py
+++ Modulo-03/ex43.py
@@ -1,8 +1,12 @@
+lista_2 = []
+
 for x in range(10):
     numero = int(input('Digite um numero: '))
+    lista_2.append(numero)
 
     if x == 0:
         lista = [numero]
+        continue
     elif numero >= lista[len(lista) - 1]:
         lista.append(numero)
         continue
@@ -14,7 +18,10 @@ for x in range(10):
         lista.insert(y, numero)
         break
 
+lista_2.sort()
+
 print(f'Maior é: {lista[len(lista) -1]}, menor é: {lista[0]}, media é: {sum(lista) / len(lista)} e a lista ordenada é: {lista}')
+print(f'Maior é: {lista_2[len(lista_2) -1]}, menor é: {lista_2[0]}, media é: {sum(lista_2) / len(lista_2)} e a lista_2 ordenada é: {lista_2}')
 
     
 
