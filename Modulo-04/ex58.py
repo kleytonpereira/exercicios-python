4f07ccd9429d763e37454d60e200e532fdd74034
Kleyton Ferreira <kleyton.ferreira.kp@gmail.com>
2026-07-20 09:52:26 -0300

    Modulo-04: fix ex58

--- Modulo-04/ex58.py
+++ Modulo-04/ex58.py
@@ -1,10 +1,8 @@
 def celsius_p_f(c):
-    print (c * 1.8 + 32)
     return round(c * 1.8 + 32)
 
 def f_p_celsius(f):
-    print ((f - 32) * (5 / 9))
     return round((f - 32) * (5 / 9))
 
 
-assert 21 == int(f_p_celsius(celsius_p_f(21))), 'Conversão correta'
+assert 21 == f_p_celsius(celsius_p_f(21)), 'Conversão correta'
