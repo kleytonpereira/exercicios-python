8ae234ed3dec1b2e781f730258780b9bb25bcf39
Kleyton Ferreira <kleyton.ferreira.kp@gmail.com>
2026-07-20 17:30:13 -0300

    Remove pasta Modulo-2 duplicada

--- Modulo-2/ex19.py
+++
@@ -1,8 +0,0 @@
-for crescente in range(0, 101):
-    print(f'{crescente} \n')
-
-for pares in range(0, 101, 2):
-    print(f'{pares} \n')
-
-for decrecente in range(100, -1, -1):
-    print(f'{decrecente} \n')
--- Modulo-2/ex20.py
+++
@@ -1,7 +0,0 @@
-numero = int(input())
-print('\n')
-MULTI = numero
-
-for x in range(10):
-    print(f'{numero} \n')
-    numero += MULTI
