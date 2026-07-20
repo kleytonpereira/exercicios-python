8446c2fde8f5bcea3d85bef27a8db3d5aebaf194
Kleyton Ferreira <kleyton.ferreira.kp@gmail.com>
2026-07-20 10:18:44 -0300

    Modulo-04: fix ex61

---
+++ Modulo-04/ex61.py
@@ -0,0 +1,15 @@
+def fatorial(numero):
+    for fatorial in range (numero-1, 0, -1):
+        numero *= fatorial
+    return numero
+
+def fatorial_rec(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    
+    return fatorial_rec(n - 1) * n
+
+
+assert fatorial(5) == 120, 'Calculado correto'
