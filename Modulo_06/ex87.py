2907b71ddd62b7761d0f0b95da0a5c6ddff73c8a
Kleyton Ferreira <kleyton.ferreira.kp@gmail.com>
2026-07-23 09:30:58 -0300

    Modulo-06: ex88

--- Modulo_06/ex87.py
+++ Modulo_06/ex87.py
@@ -1,12 +1,8 @@
 class Risco():
-    def __init__(self, descricao, categoria, impacto):
+    def __init__(self, categoria, descricao, impacto):
         self.descricao = descricao
         self.categoria = categoria
-        self.impacto = impacto
+        self.impacto = int(impacto)
 
     def __str__(self):
         return f'A categoria: {self.categoria}\nA descrição: {self.descricao}\nO impacto: {self.impacto}'
-
-
-r = Risco('Uma pequena descrição', 'Tecnico', 'Atraso de homologação')
-print(r)
